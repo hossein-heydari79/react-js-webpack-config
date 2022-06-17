326fb4169f4cdf36399b084695f083a51e32f2c1
hossein-heydari <hosseinheydari790228@gmail.com>
2022-06-17 14:41:30 +0430

    add app.js

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./_app.scss";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+          React Page {" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
